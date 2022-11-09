df446d0aeadf509427cc2ebe3e7636c8a725120b
Priyanshu Bhandari <technopriyu@gmail.com>
2022-11-09 12:23:47 +0530

    initialized next with ts tw

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+import network from '../utils/network';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // blockchain have different networks, mainnets and testnets and they alsohave nested networks
+    <ThirdwebProvider desiredChainId={network}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
